9107dbdf94826864eb7841cb8a803256182810f4
Aakash Adesara <aakash.adesara@gmail.com>
2022-09-27 15:12:31 -0500

    Fix GET /prospect

--- integrations/clay_run/clay_run_client.py
+++ integrations/clay_run/clay_run_client.py
@@ -7,25 +7,25 @@ from src.utils.crypto_box import CryptoBox
 
 
 class ClayRunClient:
-    _URL_WORKSPACE_INFO = 'https://api.clay.run/v3/workspaces/{workspace_id}'
-    _URL_CREATE_TABLE = 'https://api.clay.run/v3/tables'
-    _URL_TABLE_RECORD = 'https://api.clay.run/v3/tables/{table_id}'
+    _URL_WORKSPACE_INFO = "https://api.clay.run/v3/workspaces/{workspace_id}"
+    _URL_CREATE_TABLE = "https://api.clay.run/v3/tables"
+    _URL_TABLE_RECORD = "https://api.clay.run/v3/tables/{table_id}"
     _URL_RECORDS_IN_VIEW = (
-        'https://api.clay.run/v3/tables/{table_id}/views/{view_id}/records'
+        "https://api.clay.run/v3/tables/{table_id}/views/{view_id}/records"
     )
-    _URL_TABLE_RECORDS = 'https://api.clay.run/v3/tables/{table_id}/records'
-    _URL_TABLE_FIELDS = 'https://api.clay.run/v3/tables/{table_id}/fields'
+    _URL_TABLE_RECORDS = "https://api.clay.run/v3/tables/{table_id}/records"
+    _URL_TABLE_FIELDS = "https://api.clay.run/v3/tables/{table_id}/fields"
 
-    _URL_SOURCES = 'https://api.clay.run/v3/sources'
+    _URL_SOURCES = "https://api.clay.run/v3/sources"
 
     _MAX_RECORDS_TO_PULL = 500
     _MAX_RECORDS_TO_PROCESS = 230
 
     def __init__(self) -> None:
-        self._workspace_id = '6487'
+        self._workspace_id = "6487"
 
-        cookie = CryptoBox().decrypt_from_file('bags/clay_run.cookie.bag')
-        self._auth = CookieValueAuthBase('claysession', cookie)
+        cookie = CryptoBox().decrypt_from_file("bags/clay_run.cookie.bag")
+        self._auth = CookieValueAuthBase("claysession", cookie)
 
     def workspace_id(self) -> str:
         return self._workspace_id
@@ -46,7 +46,7 @@ class ClayRunClient:
 
     def delete_all_tables_in_workspace(self):
         tables = self.get_tables_in_workspace()
-        for table_id in deep_get(tables, 'tables').keys():
+        for table_id in deep_get(tables, "tables").keys():
             self.delete_table_in_workspace(table_id)
 
     def create_table(self, title: str):
@@ -54,12 +54,12 @@ class ClayRunClient:
             self._URL_CREATE_TABLE,
             auth=self._auth,
             json={
-                'icon': {'emoji': '🧙🏻‍♂️'},
-                'workspaceId': self._workspace_id,
-                'type': 'people',
-                'template': 'people_basic',
-                'parentFolderId': None,
-                'name': title,
+                "icon": {"emoji": "🧙🏻‍♂️"},
+                "workspaceId": self._workspace_id,
+                "type": "people",
+                "template": "people_basic",
+                "parentFolderId": None,
+                "name": title,
             },
         )
         response.raise_for_status()
@@ -69,12 +69,12 @@ class ClayRunClient:
         table_details = self._get_table_details(table_id)
         record_ids = list(
             map(
-                lambda x: deep_get(x, 'id'),
+                lambda x: deep_get(x, "id"),
                 deep_get(
                     self.get_records_in_view(
-                        table_id=table_id, view_id=deep_get(table_details, 'views.0.id')
+                        table_id=table_id, view_id=deep_get(table_details, "views.0.id")
                     ),
-                    'results',
+                    "results",
                 ),
             )
         )
@@ -82,13 +82,14 @@ class ClayRunClient:
         response = requests.delete(
             self._URL_TABLE_RECORDS.format(table_id=table_id),
             auth=self._auth,
-            json={'recordIds': record_ids},
+            json={"recordIds": record_ids},
         )
         response.raise_for_status()
 
     def _get_table_details(self, table_id: str):
         response = requests.get(
-            self._URL_TABLE_RECORD.format(table_id=table_id), auth=self._auth,
+            self._URL_TABLE_RECORD.format(table_id=table_id),
+            auth=self._auth,
         )
         response.raise_for_status()
         return response.json()
@@ -99,7 +100,7 @@ class ClayRunClient:
         response = requests.get(
             self._URL_RECORDS_IN_VIEW.format(table_id=table_id, view_id=view_id),
             auth=self._auth,
-            params={'limit': self._MAX_RECORDS_TO_PULL},
+            params={"limit": self._MAX_RECORDS_TO_PULL},
         )
         response.raise_for_status()
 
@@ -111,36 +112,36 @@ class ClayRunClient:
             self._URL_TABLE_RECORD.format(table_id=table_id),
             auth=self._auth,
             json={
-                'sourceSettings': {
-                    'addSourceField': {
-                        'name': 'San Francisco CTO LinkedIn Profiles',
-                        'sourceId': source_id,
-                        'isPinned': False,
+                "sourceSettings": {
+                    "addSource": {
+                        "name": "LinkedIn Profiles",
+                        "sourceId": source_id,
+                        "isPinned": False,
                     },
-                    'newActionFields': [
+                    "newActionFields": [
                         {
-                            'type': 'action',
-                            'name': 'Enrich Person',
-                            'typeSettings': {
-                                'actionKey': 'enrich-person-with-coresignal',
-                                'actionVersion': 1,
-                                'actionPackageId': 'e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2',
-                                'inputsBinding': [
+                            "type": "action",
+                            "name": "Enrich Person",
+                            "typeSettings": {
+                                "actionKey": "enrich-person-with-coresignal",
+                                "actionVersion": 1,
+                                "actionPackageId": "e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2",
+                                "inputsBinding": [
                                     {
-                                        'name': 'member_url',
-                                        'formulaText': '{{ source }}.id',
+                                        "name": "member_url",
+                                        "formulaText": "{{ source }}.id",
                                     }
                                 ],
-                                'dataTypeSettings': {'type': 'json'},
+                                "dataTypeSettings": {"type": "json"},
                             },
-                            'fieldId': 'f_source_enrich_person',
-                            'isPinned': True,
+                            "fieldId": "f_source_enrich_person",
+                            "isPinned": True,
                         }
                     ],
-                    'formulaOverrides': [
+                    "formulaOverrides": [
                         {
-                            'fieldIdOrEnum': 'f_person_linkedin_url',
-                            'formulaText': '{{ action0 }}.canonical_url',
+                            "fieldIdOrEnum": "f_person_linkedin_url",
+                            "formulaText": "{{ action0 }}.canonical_url",
                         }
                     ],
                 }
@@ -153,105 +154,105 @@ class ClayRunClient:
             self._URL_SOURCES,
             auth=self._auth,
             json={
-                'name': f'{source.location} {source.headline} LinkedIn Profiles',
-                'workspaceId': self._workspace_id,
-                'type': 'v3-action',
-                'typeSettings': {
-                    'name': 'LinkedIn Profiles',
-                    'description': 'Query LinkedIn data to find people.',
-                    'iconType': 'LinkedInSource',
-                    'actionKey': 'find-member-ids-with-coresignal-source',
-                    'previewActionKey': 'find-member-ids-with-coresignal-source',
-                    'actionPackageId': 'e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2',
-                    'recordsPath': 'ids',
-                    'defaultPreviewText': 'LinkedIn Profile',
-                    'idPath': 'id',
-                    'isAdmin': False,
-                    'sourceTableOutputs': {
-                        'people': {
-                            'addOrUpdateActionFields': [
+                "name": f"{source.location} {source.headline} LinkedIn Profiles",
+                "workspaceId": self._workspace_id,
+                "type": "v3-action",
+                "typeSettings": {
+                    "name": "LinkedIn Profiles",
+                    "description": "Query LinkedIn data to find people.",
+                    "iconType": "LinkedInSource",
+                    "actionKey": "find-member-ids-with-coresignal-source",
+                    "previewActionKey": "find-member-ids-with-coresignal-source",
+                    "actionPackageId": "e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2",
+                    "recordsPath": "ids",
+                    "defaultPreviewText": "LinkedIn Profile",
+                    "idPath": "id",
+                    "isAdmin": False,
+                    "sourceTableOutputs": {
+                        "people": {
+                            "addOrUpdateActionFields": [
                                 {
-                                    'name': 'Enrich Person',
-                                    'actionVersion': 1,
-                                    'actionPackageId': 'e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2',
-                                    'actionKey': 'enrich-person-with-coresignal',
-                                    'inputMappings': [
-                                        {'name': 'member_url', 'path': ['id']}
+                                    "name": "Enrich Person",
+                                    "actionVersion": 1,
+                                    "actionPackageId": "e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2",
+                                    "actionKey": "enrich-person-with-coresignal",
+                                    "inputMappings": [
+                                        {"name": "member_url", "path": ["id"]}
                                     ],
-                                    'actionOutputMappings': [
+                                    "actionOutputMappings": [
                                         {
-                                            'path': ['canonical_url'],
-                                            'mapToFieldId': 'f_person_linkedin_url',
+                                            "path": ["canonical_url"],
+                                            "mapToFieldId": "f_person_linkedin_url",
                                         }
                                     ],
-                                    'fieldId': 'f_source_enrich_person',
-                                    'isPinned': True,
+                                    "fieldId": "f_source_enrich_person",
+                                    "isPinned": True,
                                 }
                             ]
                         },
-                        'peoplev2': {
-                            'addOrUpdateActionFields': [
+                        "peoplev2": {
+                            "addOrUpdateActionFields": [
                                 {
-                                    'name': 'Enrich Person',
-                                    'actionVersion': 1,
-                                    'actionPackageId': 'e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2',
-                                    'actionKey': 'enrich-person-with-coresignal',
-                                    'inputMappings': [
-                                        {'name': 'member_url', 'path': ['id']}
+                                    "name": "Enrich Person",
+                                    "actionVersion": 1,
+                                    "actionPackageId": "e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2",
+                                    "actionKey": "enrich-person-with-coresignal",
+                                    "inputMappings": [
+                                        {"name": "member_url", "path": ["id"]}
                                     ],
-                                    'actionOutputMappings': [
+                                    "actionOutputMappings": [
                                         {
-                                            'path': ['canonical_url'],
-                                            'mapToFieldId': 'person/linkedIn/url',
+                                            "path": ["canonical_url"],
+                                            "mapToFieldId": "person/linkedIn/url",
                                         }
                                     ],
-                                    'fieldId': 'f_source_enrich_person',
-                                    'isPinned': True,
+                                    "fieldId": "f_source_enrich_person",
+                                    "isPinned": True,
                                 }
                             ]
                         },
-                        'spreadsheet': {
-                            'addOrUpdateActionFields': [
+                        "spreadsheet": {
+                            "addOrUpdateActionFields": [
                                 {
-                                    'name': 'Enrich Person',
-                                    'actionVersion': 1,
-                                    'actionPackageId': 'e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2',
-                                    'actionKey': 'enrich-person-with-coresignal',
-                                    'inputMappings': [
-                                        {'name': 'member_url', 'path': ['id']}
+                                    "name": "Enrich Person",
+                                    "actionVersion": 1,
+                                    "actionPackageId": "e251a70e-46d7-4f3a-b3ef-a211ad3d8bd2",
+                                    "actionKey": "enrich-person-with-coresignal",
+                                    "inputMappings": [
+                                        {"name": "member_url", "path": ["id"]}
                                     ],
-                                    'fieldId': 'f_source_enrich_person',
-                                    'isPinned': True,
+                                    "fieldId": "f_source_enrich_person",
+                                    "isPinned": True,
                                 }
                             ]
                         },
                     },
-                    'stages': ['Inputs'],
-                    'tableNameInputFields': ['member_location', 'title'],
-                    'isSourcePinned': False,
-                    'inputs': {
-                        'member_location': f"\"{source.location}\"",
-                        'limit': f"\"{self._MAX_RECORDS_TO_PROCESS}\"",
-                        'title': f"\"{source.headline}\"",
-                        'member_experience_title': f"\"{source.headline}\"",
-                        'member_industry': f"\"{source.industry}\"",
+                    "stages": ["Inputs"],
+                    "tableNameInputFields": ["member_location", "title"],
+                    "isSourcePinned": False,
+                    "inputs": {
+                        "member_location": f'"{source.location}"',
+                        "limit": f'"{self._MAX_RECORDS_TO_PROCESS}"',
+                        "title": f'"{source.headline}"',
+                        "member_experience_title": f'"{source.headline}"',
+                        "member_industry": f'"{source.industry}"',
                     },
                 },
             },
         )
         response.raise_for_status()
-        return deep_get(response.json(), 'id')
+        return deep_get(response.json(), "id")
 
     def add_bio_to_table(self, table_id: str):
         response = requests.post(
             self._URL_TABLE_FIELDS.format(table_id=table_id),
             auth=self._auth,
             json={
-                'type': 'people',
-                'name': 'Bio',
-                'typeSettings': {
-                    'dataTypeSettings': {'type': 'text'},
-                    'peopleAttribute': 'PERSON_BIO',
+                "type": "people",
+                "name": "Bio",
+                "typeSettings": {
+                    "dataTypeSettings": {"type": "text"},
+                    "peopleAttribute": "PERSON_BIO",
                 },
             },
         )
