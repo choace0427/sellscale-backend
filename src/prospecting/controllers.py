84123badd0720836d6d6c20100b8e5a831edcefd
Aakash Adesara <aakash.adesara@gmail.com>
2022-09-28 12:42:43 -0500

    Pip Freeze Requirements

--- src/prospecting/controllers.py
+++ src/prospecting/controllers.py
@@ -1,24 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src.prospecting.clay_run.clay_run_prospector import ClayRunProspector
 from src.prospecting.clay_run.configs import ProspectingConfig
+from src.utils.request_helpers import get_request_parameter
 
-PROSPECTING_BLUEPRINT = Blueprint('prospect', __name__)
+PROSPECTING_BLUEPRINT = Blueprint("prospect", __name__)
 
 
 @PROSPECTING_BLUEPRINT.route("/")
 def index():
+    location = get_request_parameter("location", request, json=True, required=True)
+    headline = get_request_parameter("headline", request, json=True, required=True)
+    industry = get_request_parameter("industry", request, json=True, required=True)
+    experience = get_request_parameter("experience", request, json=True, required=True)
+
     prospector: ClayRunProspector = ClayRunProspector()
 
     config: ProspectingConfig = ProspectingConfig(
-        location='San Francisco',
-        headline='CTO',
-        industry='Technology',
-        experience='Technology'
+        location=location, headline=headline, industry=industry, experience=experience
     )
 
-    # data = []
-    data = prospector.prospect_sync(
-        prospecting_config=config
-    )
+    data = prospector.prospect_sync(prospecting_config=config)
 
-    return jsonify({'data': data})
+    print(data)
+
+    return jsonify({"data": data})
---
+++ src/prospecting/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class Prospect(db.Model):
+    __tablename__ = "prospect"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    archetype_id = db.Column(db.Integer, db.ForeignKey("client_archetype.id"))
+    company = db.Column(db.String, nullable=True)
+    company_url = db.Column(db.String, nullable=True)
+    full_name = db.Column(db.String, nullable=True)
+    industry = db.Column(db.String, nullable=True)
+    linkedin_url = db.Column(db.String, nullable=True)
+    linkedin_bio = db.Column(db.String, nullable=True)
+    title = db.Column(db.String, nullable=True)
+    twitter_url = db.Column(db.String, nullable=True)
