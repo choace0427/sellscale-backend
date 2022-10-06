55990708bac6eebdb73c7b7f06fcec7761e041e3
Aakash Adesara <aakash.adesara@gmail.com>
2022-10-06 13:49:22 -0700

    Fixed Timestamped Model

--- src/setup/TimestampedModel.py
+++ src/setup/TimestampedModel.py
@@ -2,6 +2,9 @@ from flask_sqlalchemy import Model
 from sqlalchemy import Column, DateTime
 import datetime
 
+
 class TimestampedModel(Model):
-    created_at = Column(DateTime, default=datetime.datetime.now())
-    updated_at = Column(DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now
+    )
