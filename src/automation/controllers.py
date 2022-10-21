e7099e9cafc60b539e60ac53ddf9c6c4c7532749
Aakash Adesara <aakash.adesara@gmail.com>
2022-10-21 14:54:55 -0700

    Added Phantom Inbox Scraper Config

--- src/automation/controllers.py
+++ src/automation/controllers.py
@@ -1,5 +1,6 @@
 import json
 from flask import Blueprint, request, jsonify
+from src.automation.models import PhantomBusterType
 from src.client.models import ClientArchetype
 from src.client.services import (
     create_client,
@@ -39,6 +40,31 @@ def post_phantom_buster_config():
         client_id=client_id,
         client_sdr_id=client_sdr_id,
         google_sheets_uuid=google_sheets_uuid,
+        phantom_type=PhantomBusterType.OUTBOUND_ENGINE,
+        phantom_name=phantom_name,
+        phantom_uuid=phantom_uuid,
+    )
+
+    return jsonify(resp)
+
+
+@AUTOMATION_BLUEPRINT.route("/create/inbox_scraper_config", methods=["POST"])
+def post_inbox_scraper_config():
+    client_id = get_request_parameter("client_id", request, json=True, required=True)
+    client_sdr_id = get_request_parameter(
+        "client_sdr_id", request, json=True, required=True
+    )
+    phantom_name = get_request_parameter(
+        "phantom_name", request, json=True, required=True
+    )
+    phantom_uuid = get_request_parameter(
+        "phantom_uuid", request, json=True, required=True
+    )
+
+    resp = create_phantom_buster_config(
+        client_id=client_id,
+        client_sdr_id=client_sdr_id,
+        phantom_type=PhantomBusterType.INBOX_SCRAPER,
         phantom_name=phantom_name,
         phantom_uuid=phantom_uuid,
     )
--- src/automation/services.py
+++ src/automation/services.py
@@ -11,13 +11,14 @@ GET_PHANTOMBUSTER_AGENTS_URL = "https://api.phantombuster.com/api/v2/agents/fetc
 def create_phantom_buster_config(
     client_id: int,
     client_sdr_id: int,
-    google_sheets_uuid: str,
     phantom_name: str,
     phantom_uuid: str,
+    phantom_type: PhantomBusterType,
+    google_sheets_uuid: str = None,
 ):
     existing_config: PhantomBusterConfig = PhantomBusterConfig.query.filter(
         PhantomBusterConfig.client_sdr_id == client_sdr_id,
-        PhantomBusterConfig.pb_type == PhantomBusterType.OUTBOUND_ENGINE,
+        PhantomBusterConfig.pb_type == phantom_type,
     ).first()
     if existing_config:
         return {"phantom_buster_config_id": existing_config.id}
@@ -26,6 +27,7 @@ def create_phantom_buster_config(
         client_id=client_id,
         client_sdr_id=client_sdr_id,
         google_sheets_uuid=google_sheets_uuid,
+        pb_type=phantom_type,
         phantom_name=phantom_name,
         phantom_uuid=phantom_uuid,
     )
