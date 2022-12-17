7c63ce97a0a5fb39ed3c30ec799b1e1eecb2d545
Aakash Adesara <aakash.adesara@gmail.com>
2022-12-16 21:09:18 -0800

    POST /clear_message_generation_jobs_queue

--- tests/message_generation/test_message_generation_controllers.py
+++ tests/message_generation/test_message_generation_controllers.py
@@ -18,6 +18,7 @@ from model_import import (
     GeneratedMessage,
     GeneratedMessageStatus,
     GeneratedMessageFeedback,
+    GeneratedMessageJobStatus,
 )
 from src.research.models import ResearchPointType, ResearchType
 from src.client.services import create_client
@@ -73,3 +74,30 @@ def test_post_generate_ai_made_ctas(open_ai_completion_mock):
     assert response.status_code == 200
     assert len(response.json["ctas"]) == 1
     assert open_ai_completion_mock.call_count == 1
+
+
+@use_app_context
+def test_post_clear_message_generation_jobs_queue():
+    client = basic_client()
+    archetype = basic_archetype(client)
+    prospect = basic_prospect(client=client, archetype=archetype)
+    for i in range(10):
+        generated_message_job = GeneratedMessageJob(
+            prospect_id=prospect.id,
+            batch_id="123123",
+            status=GeneratedMessageJobStatus.PENDING,
+        )
+        db.session.add(generated_message_job)
+        db.session.commit()
+
+    gm_jobs = GeneratedMessageJob.query.all()
+    assert len(gm_jobs) == 10
+
+    response = app.test_client().post(
+        "message_generation/clear_message_generation_jobs_queue",
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status_code == 200
+
+    gm_jobs = GeneratedMessageJob.query.all()
+    assert len(gm_jobs) == 0
