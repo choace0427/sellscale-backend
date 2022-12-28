6f1aaf7e1e66adf87f13443e8a6f5ec16d8224a5
Aakash Adesara <aakash.adesara@gmail.com>
2022-12-28 11:47:46 -0800

    Fix Co Description Transformer

--- src/research/linkedin/extractors/current_company.py
+++ src/research/linkedin/extractors/current_company.py
@@ -9,19 +9,17 @@ def get_current_company_description(data):
     company_description = sanitize_string(
         data.get("company", {}).get("details", {}).get("description")
     )
-    tagline = sanitize_string(data.get("company", {}).get("details", {}).get("tagline"))
 
     raw_data = {
         "company_name": company_name,
         "company_description": company_description,
-        "tagline": tagline,
     }
 
-    prompt = "company: {company_name} -- description: {company_description} -- tagline: {tagline}\n -- summary:".format(
+    prompt = "company: {company_name} -- description: {company_description}\n -- summary:".format(
         **raw_data
     )
 
-    if not company_name or not company_description or not tagline:
+    if not company_name or not company_description:
         response = ""
     else:
         response = get_completion(bullet_model_id="recent_job_summary", prompt=prompt)
