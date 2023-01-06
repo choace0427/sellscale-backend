136dafcfe6d391bc3a5cc27764d5ad861f1bddc5
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-05 17:49:32 -0800

    Update Phantom Buster Run Statuses

--- src/automation/models.py
+++ src/automation/models.py
@@ -2,6 +2,8 @@ from email.policy import default
 from app import db
 import sqlalchemy as sa
 import enum
+import requests
+import datetime
 
 
 class PhantomBusterType(enum.Enum):
@@ -26,3 +28,43 @@ class PhantomBusterConfig(db.Model):
 
     last_run_date = db.Column(db.DateTime, nullable=True)
     error_message = db.Column(db.String, nullable=True)
+
+
+class PhantomBusterAgent:
+    FETCH_AGENT_URL = (
+        url
+    ) = "https://api.phantombuster.com/api/v2/agents/fetch?id={phantom_uuid}"
+    FETCH_AGENT_OUTPUT = (
+        "https://api.phantombuster.com/api/v2/agents/fetch-output?id={phantom_uuid}"
+    )
+
+    def __init__(self, id: int, api_key: str):
+        self.id = id
+        self.api_key = api_key
+
+    def get_last_run_date(self):
+        url = self.FETCH_AGENT_URL.format(phantom_uuid=self.id)
+        payload = {}
+        headers = {
+            "X-Phantombuster-Key": self.api_key,
+            "accept": "application/json",
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data: dict = response.json()
+        return datetime.datetime.fromtimestamp(data.get("updatedAt") / 1000.0)
+
+    def get_error_message(self):
+        url = self.FETCH_AGENT_OUTPUT.format(phantom_uuid=self.id)
+        payload = {}
+        headers = {
+            "X-Phantombuster-Key": self.api_key,
+            "accept": "application/json",
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        output = data.get("output")
+
+        if "Session cookie not valid" in output:
+            return "Session cookie not valid anymore. Please update the cookie."
+        return None
