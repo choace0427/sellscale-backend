acbc6c2045fa8ddfe50567f593cc1ae46bad9860
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-10 12:36:48 -0800

    Transformer Adjustment Endpoints

--- src/automation/controllers.py
+++ src/automation/controllers.py
@@ -1,16 +1,9 @@
 import json
 from flask import Blueprint, request, jsonify
 from src.automation.models import PhantomBusterType
-from src.client.models import ClientArchetype
-from src.client.services import (
-    create_client,
-    create_client_archetype,
-    create_client_sdr,
-)
 from src.automation.services import create_phantom_buster_config
 from src.automation.services import get_all_phantom_busters
 from src.utils.request_helpers import get_request_parameter
-from src.utils.slack import send_slack_message
 from src.automation.inbox_scraper import scrape_inbox
 from src.automation.services import create_new_auto_connect_phantom
 
