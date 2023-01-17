f789189c56fe5c20001f148441df9a53193167df
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-16 21:32:32 -0800

    LI Conversation V2

--- src/li_conversation/controllers.py
+++ src/li_conversation/controllers.py
@@ -1,19 +1,27 @@
 import json
 from flask import Blueprint, request, jsonify
 from flask_csv import send_csv
+from model_import import Prospect
+from datetime import datetime, timedelta
 
 LI_CONVERASTION_BLUEPRINT = Blueprint("li_conversation", __name__)
 
 
 @LI_CONVERASTION_BLUEPRINT.route("/")
 def get_li_conversation():
+    prospects = Prospect.query.filter(
+        Prospect.client_id == 9,
+        Prospect.li_last_message_timestamp > datetime.now() - timedelta(days=24),
+    ).all()
+
+    linkedin_urls = [
+        {"linkedin_url": prospect.linkedin_url}
+        for prospect in prospects
+        if prospect.linkedin_url
+    ]
 
     return send_csv(
-        [
-            {"linkedin_url": "https://www.linkedin.com/in/abedelmalik/"},
-            {"linkedin_url": "https://www.linkedin.com/in/dhavalbhatt/"},
-            {"linkedin_url": "https://www.linkedin.com/in/randallrhall/"},
-        ],
+        linkedin_urls,
         "test.csv",
         ["linkedin_url"],
     )
