5bec4dffe069c66b1d10dd84a7f6bb3602579e1b
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-20 11:49:40 -0800

    Added POST /li_conversation/update_linkedin_conversation_entries

--- src/li_conversation/controllers.py
+++ src/li_conversation/controllers.py
@@ -3,12 +3,23 @@ from flask import Blueprint, request, jsonify
 from flask_csv import send_csv
 from model_import import Prospect
 from datetime import datetime, timedelta
+from model_import import ClientSDR
+from src.li_conversation.services import update_linkedin_conversation_entries
 
 LI_CONVERASTION_BLUEPRINT = Blueprint("li_conversation", __name__)
 
 
+@LI_CONVERASTION_BLUEPRINT.route(
+    "/update_linkedin_conversation_entries", methods=["POST"]
+)
+def post_update_linkedin_conversation_entries():
+    update_linkedin_conversation_entries()
+    return "OK", 200
+
+
 @LI_CONVERASTION_BLUEPRINT.route("/<client_id>")
-def get_li_conversation(client_id):
+def get_li_conversation_csv(client_id):
+    """Returns a CSV of prospects who have had a LinkedIn conversation extracted in the last 24 hours."""
     prospects = Prospect.query.filter(
         Prospect.client_id == client_id,
         Prospect.li_last_message_timestamp > datetime.now() - timedelta(days=1),
@@ -25,3 +36,10 @@ def get_li_conversation(client_id):
         "test.csv",
         ["linkedin_url"],
     )
+
+
+@LI_CONVERASTION_BLUEPRINT.route("/sdr/<client_sdr_id>", methods=["POST"])
+def post_client_sdr_li_conversation_arguments(client_sdr_id):
+    """Saves the LinkedIn conversation arguments for a given SDR."""
+
+    return "OK", 200
