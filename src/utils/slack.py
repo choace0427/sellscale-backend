331bc901ea9aff91378e217be833b69569be4b59
Aakash Adesara <aakash.adesara@gmail.com>
2023-02-20 17:40:12 -0800

    Voice Update Requests

--- src/utils/slack.py
+++ src/utils/slack.py
@@ -5,6 +5,7 @@ URL_MAP = {
     "eng-sandbox": "https://hooks.slack.com/services/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",
     "sellscale_pipeline_all_clients": "https://hooks.slack.com/services/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",
     "operations-ready-campaigns": "https://hooks.slack.com/services/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",
+    "operations-voice-update-requests": "https://hooks.slack.com/services/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",
 }
 
 
