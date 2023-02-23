b1d48ec41223961f68fa52d7e51e63256e813fa0
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-02-23 10:27:03 -0800

    Create ae75a0354351_merge_heads.py (#413)

---
+++ migrations/versions/ae75a0354351_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: ae75a0354351
+Revises: f7514eb88322, 560ecd9e8a0a
+Create Date: 2023-02-23 10:25:54.394019
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ae75a0354351'
+down_revision = ('f7514eb88322', '560ecd9e8a0a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
