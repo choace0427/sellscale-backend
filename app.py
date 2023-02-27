9e781de59077803a7274011cd6cd60050e2619bd
Aakash Adesara <aakash.adesara@gmail.com>
2023-02-26 20:08:04 -0800

    Auto Enabling Integrations

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ sentry_sdk.init(
     integrations=[
         FlaskIntegration(),
     ],
+    auto_enabling_integrations=False,
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
