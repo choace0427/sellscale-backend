1260a952bc8ac8f9263e740f25110d53fafdee4e
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-03-01 11:18:40 -0800

    Modify Pipeline Endpoint to return ALL statuses (#461)
    * Modify pipeline endpoint to return overall status and test
    
    * Update test_analytics_controller.py
    
    * Return all statuses and test

--- src/analytics/services.py
+++ src/analytics/services.py
@@ -2,9 +2,11 @@ from app import db
 from flask import jsonify
 
 from src.client.models import *
-from datetime import datetime, timedelta
 from src.message_generation.models import *
 from src.prospecting.models import *
+from src.email_outbound.models import *
+
+from datetime import datetime, timedelta
 
 
 def get_all_latest_week_benchmarks_for_clients():
@@ -198,14 +200,44 @@ def get_sdr_pipeline_all_details(client_sdr_id: int) -> dict:
     Returns:
         dict: Returns a dict of ProspectStatus details for a given ClientSDR
     """
-    statuses_count = {}
-    for status in ProspectStatus.all_statuses():
-        statuses_count[status.value.lower()] = (
+    all_pipeline_details = {}
+
+    # Get LinkedIn Statuses
+    li_statuses_count = {}
+    for li_status in ProspectStatus.all_statuses():
+        li_statuses_count[li_status.value.lower()] = (
             Prospect.query.filter(
                 Prospect.client_sdr_id == client_sdr_id,
-                Prospect.status == status,
+                Prospect.status == li_status,
             )
             .count()
         )
+    all_pipeline_details.update(li_statuses_count)          # TODO REMOVE THIS
+    all_pipeline_details[ProspectChannels.LINKEDIN.value] = li_statuses_count
 
-    return statuses_count
+    # Get Overall Statuses
+    overall_statuses_count = {}
+    for overall_status in ProspectOverallStatus.all_statuses():
+        overall_statuses_count[overall_status.value] = (
+            Prospect.query.filter(
+                Prospect.client_sdr_id == client_sdr_id,
+                Prospect.overall_status == overall_status,
+            )
+            .count()
+        )
+    all_pipeline_details[ProspectChannels.SELLSCALE.value] = overall_statuses_count
+
+    # Get Email Statuses
+    email_statuses_count = {}
+    prospect_ids = [p.id for p in Prospect.query.filter(Prospect.client_sdr_id==client_sdr_id).all()]
+    for email_status in ProspectEmailOutreachStatus.all_statuses():
+        email_statuses_count[email_status.value] = (
+            ProspectEmail.query.filter(
+                ProspectEmail.prospect_id.in_(prospect_ids),
+                ProspectEmail.outreach_status == email_status,
+            )
+            .count()
+        )
+    all_pipeline_details[ProspectChannels.EMAIL.value] = email_statuses_count
+
+    return all_pipeline_details
--- tests/analytics/test_analytics_services.py
+++ tests/analytics/test_analytics_services.py
@@ -7,7 +7,7 @@ from test_utils import (
     basic_prospect,
     basic_archetype
 )
-from model_import import ProspectStatus
+from model_import import ProspectStatus, ProspectOverallStatus
 from src.analytics.services import get_sdr_pipeline_all_details
 
 import json
@@ -18,24 +18,58 @@ def test_get_sdr_pipeline_all_details():
     client = basic_client()
     client_sdr = basic_client_sdr(client)
     archetype = basic_archetype(client)
-    prospect = basic_prospect(client, archetype, client_sdr, status=ProspectStatus.SENT_OUTREACH)
-    prospect_2 = basic_prospect(client, archetype, client_sdr, status=ProspectStatus.SENT_OUTREACH)
-    prospect_3 = basic_prospect(client, archetype, client_sdr, status=ProspectStatus.ACCEPTED)
-    prospect_4 = basic_prospect(client, archetype, client_sdr, status=ProspectStatus.RESPONDED)
-    prospect_5 = basic_prospect(client, archetype, client_sdr, status=ProspectStatus.NOT_INTERESTED)
+    prospect = basic_prospect(client, archetype, client_sdr, overall_status=ProspectOverallStatus.SENT_OUTREACH)
+    prospect_2 = basic_prospect(client, archetype, client_sdr, overall_status=ProspectOverallStatus.SENT_OUTREACH)
+    prospect_3 = basic_prospect(client, archetype, client_sdr, overall_status=ProspectOverallStatus.ACCEPTED)
+    prospect_4 = basic_prospect(client, archetype, client_sdr, overall_status=ProspectOverallStatus.ACTIVE_CONVO)
+    prospect_5 = basic_prospect(client, archetype, client_sdr, overall_status=ProspectOverallStatus.REMOVED)
 
     details = get_sdr_pipeline_all_details(client_sdr_id=client_sdr.id)
     assert details == {
-        "prospected": 0,
-        "not_qualified": 0,
-        "sent_outreach": 2,
-        "accepted": 1,
-        "responded": 1,
-        "active_convo": 0,
-        "scheduling": 0,
-        "not_interested": 1,
-        "demo_set": 0,
-        "demo_won": 0,
-        "demo_loss": 0,
+        'prospected': 5,
+        'not_qualified': 0,
+        'sent_outreach': 0,
+        'accepted': 0,
+        'responded': 0,
+        'active_convo': 0,
+        'scheduling': 0,
+        'not_interested': 0,
+        'demo_set': 0,
+        'demo_won': 0,
+        'demo_loss': 0,
+        'LINKEDIN': {
+            'prospected': 5,
+            'not_qualified': 0,
+            'sent_outreach': 0,
+            'accepted': 0,
+            'responded': 0,
+            'active_convo': 0,
+            'scheduling': 0,
+            'not_interested': 0,
+            'demo_set': 0,
+            'demo_won': 0,
+            'demo_loss': 0
+        },
+        'SELLSCALE': {
+            'PROSPECTED': 0,
+            'SENT_OUTREACH': 2,
+            'ACCEPTED': 1,
+            'BUMPED': 0,
+            'ACTIVE_CONVO': 1,
+            'DEMO': 0,
+            'REMOVED': 1
+        },
+        'EMAIL': {
+            'UNKNOWN': 0,
+            'NOT_SENT': 0,
+            'SENT_OUTREACH': 0,
+            'EMAIL_OPENED': 0,
+            'ACCEPTED': 0,
+            'ACTIVE_CONVO': 0,
+            'SCHEDULING': 0,
+            'NOT_INTERESTED': 0,
+            'DEMO_SET': 0,
+            'DEMO_WON': 0,
+            'DEMO_LOST': 0
+        }
     }
-
