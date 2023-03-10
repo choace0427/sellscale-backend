837a1531aea87f048e903a9cf830c76cdb4398b2
Aakash Adesara <aakash.adesara@gmail.com>
2023-03-09 18:14:51 -0800

    hotfix

--- src/message_generation/services.py
+++ src/message_generation/services.py
@@ -144,7 +144,12 @@ def research_and_generate_outreaches_for_prospect(
         try:
             prospect: Prospect = Prospect.query.get(prospect_id)
             get_research_and_bullet_points_new(prospect_id=prospect_id, test_mode=False)
-            if prospect.client_id in (1, 10, 15):  # only for SellScale & Ramp for now
+            if prospect.client_id in (
+                1,
+                10,
+                15,
+                16,
+            ):  # only for SellScale, Brex, Verkada, Ramp for now
                 generate_linkedin_outreaches_with_configurations(
                     prospect_id=prospect_id, cta_id=cta_id, batch_id=batch_id
                 )
