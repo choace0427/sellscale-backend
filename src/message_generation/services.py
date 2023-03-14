6a143e298384f784941fc81e7c9b108ba4b468b1
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-03-14 15:10:56 -0700

    Modify research point permutations to return empty list on fail (#534)

--- src/message_generation/services.py
+++ src/message_generation/services.py
@@ -248,6 +248,10 @@ def generate_batch_of_research_points_from_config(
         prospect_id=prospect_id
     )
 
+    # If there are no research points, return an empty list
+    if not all_research_points or len(all_research_points) == 0:
+        return []
+
     if not config:
         return generate_batches_of_research_points(
             points=all_research_points, n=n, num_per_perm=2
