2b01dcec578f1fbc28571a7430a44a5c0b2fb0e7
Quzzar <31683740+Quzzar@users.noreply.github.com>
2023-03-14 17:08:46 -0700

    Added cookie endpoint (#538)
    Co-authored-by: Aaron Cassar <aaron@sellscale.com>

--- src/integrations/controllers.py
+++ src/integrations/controllers.py
@@ -105,3 +105,24 @@ def post_linkedin_credentials(client_sdr_id: int):
     )
 
     return jsonify({"message": 'Sent credentials'}), 200
+
+
+@INTEGRATION_BLUEPRINT.route("/linkedin/send-cookie", methods=["POST"])
+@require_user
+def post_linkedin_cookie(client_sdr_id: int):
+    cookie = get_request_parameter("cookie", request, json=True, required=True)
+
+    client_sdr = ClientSDR.query.get(client_sdr_id)
+    client_id = client_sdr.client_id
+    client: Client = Client.query.get(client_id)
+
+    send_slack_message(
+        message="*New Cookie Submit*\nFor {client_sdr_name} from {client_company} :tada:\n\n_Cookie:_ {cookie}\n\n_Please delete this message once transferred to 1password._".format(
+          client_sdr_name=client_sdr.name,
+          client_company=client.company,
+          cookie=cookie
+        ),
+        webhook_urls=[URL_MAP["linkedin-credentials"]],
+    )
+
+    return jsonify({"message": 'Sent cookie'}), 200
