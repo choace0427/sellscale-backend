f90839639753b6574c31d422ab0661fcc787a2c5
David Wei <david.wei4043@gmail.com>
2023-04-12 16:01:14 -0500

    HOTFIX: Merge heads

---
+++ migrations/versions/faa7da6b6210_.py
@@ -0,0 +1,24 @@
+""" Merge 0b3e5880b55d and 8c4d9cfb7993
+
+Revision ID: faa7da6b6210
+Revises: 0b3e5880b55d, 8c4d9cfb7993
+Create Date: 2023-04-12 16:00:52.954025
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'faa7da6b6210'
+down_revision = ('0b3e5880b55d', '8c4d9cfb7993')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
