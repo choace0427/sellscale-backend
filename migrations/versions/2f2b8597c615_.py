154123463ffbc33b512f0a0d3988a6fb2d90a6fd
David Wei <david.wei4043@gmail.com>
2023-04-20 13:15:26 -0700

    Merge heads

---
+++ migrations/versions/2f2b8597c615_.py
@@ -0,0 +1,24 @@
+""" Merge heads
+
+Revision ID: 2f2b8597c615
+Revises: c797e5fb1cc8, ea8e8fe297bd
+Create Date: 2023-04-20 13:15:00.663635
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f2b8597c615'
+down_revision = ('c797e5fb1cc8', 'ea8e8fe297bd')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
