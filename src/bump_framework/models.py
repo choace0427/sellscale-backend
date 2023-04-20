19ee585a4ceecff4cba09b592b2d7cdb25f4c853
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-04-20 13:37:17 -0700

    Add title and default to bump_framework (#669)

--- src/bump_framework/models.py
+++ src/bump_framework/models.py
@@ -8,6 +8,7 @@ class BumpFramework(db.Model):
     __tablename__ = "bump_framework"
 
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
     active = db.Column(db.Boolean, nullable=False, default=True)
@@ -15,9 +16,13 @@ class BumpFramework(db.Model):
 
     overall_status = db.Column(db.Enum(ProspectOverallStatus), nullable=True)
 
+    default = db.Column(db.Boolean, nullable=False, default=False)
+
     def to_dict(self):
         return {
             "id": self.id,
+            "title": self.title,
             "description": self.description,
             "active": self.active,
+            "default": self.default,
         }
