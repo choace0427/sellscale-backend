eb1a20c76a78afb8ff77d90cf0af716f81c607f3
Aakash Adesara <aakash.adesara@gmail.com>
2023-04-25 16:05:31 -0700

    hotfix

--- src/client/models.py
+++ src/client/models.py
@@ -102,6 +102,7 @@ class ClientArchetype(db.Model):
             "persona_fit_reason": self.persona_fit_reason,
             "vessel_sequence_id": self.vessel_sequence_id,
             "icp_matching_prompt": self.icp_matching_prompt,
+            "is_unassigned_contact_archetype": self.is_unassigned_contact_archetype,
         }
 
 
