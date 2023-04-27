67863a8b895ede6a9462260b6383a134002796b1
Aaron Cassar <aaron@sellscale.com>
2023-04-27 15:45:01 -0700

    Hotfix

--- src/ml/services.py
+++ src/ml/services.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from src.research.models import ResearchPoints
 from src.research.models import ResearchPayload
