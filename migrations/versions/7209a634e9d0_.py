9942c77ff6414459aa161ba8e37cc3b386e900bb
David Wei <david.wei4043@gmail.com>
2023-05-09 09:35:53 -0700

    Merge heads

---
+++ migrations/versions/7209a634e9d0_.py
@@ -0,0 +1,24 @@
+""" Merge heads
+
+Revision ID: 7209a634e9d0
+Revises: 0e4658bbbfe7, cfd34f721df6
+Create Date: 2023-05-09 09:35:03.030699
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7209a634e9d0'
+down_revision = ('0e4658bbbfe7', 'cfd34f721df6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
