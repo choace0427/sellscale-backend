804d9b17e19cd8b3361a9e56969cafac31cc3927
Aaron Cassar <aaron@sellscale.com>
2023-05-22 20:42:48 -0700

    Fixed bug with calendly oauth

--- src/calendly/services.py
+++ src/calendly/services.py
@@ -4,7 +4,8 @@ from typing import Union
 import requests
 from src.client.models import ClientSDR
 from app import db
-
+import os
+import base64
 
 def update_calendly_access_token(client_sdr_id: int, code: Union[str, None] = None, refresh_token: Union[str, None] = None):
     
@@ -21,15 +22,15 @@ def update_calendly_access_token_via_code(client_sdr_id: int, code: str):
     
     sdr: ClientSDR = ClientSDR.query.get(client_sdr_id)
 
+    bytes_string = f'{os.environ.get("CALENDLY_CLIENT_ID")}:{os.environ.get("CALENDLY_CLIENT_SECRET")}'.encode('utf-8')
+    base64_string = base64.b64encode(bytes_string).decode('utf-8')
+
     res = requests.post(
-        url=f"https://auth.calendly.com/oauth/token",
+        url=f"https://auth.calendly.com/oauth/token?grant_type=authorization_code&code={code}&redirect_uri=https://app.sellscale.com/authcalendly",
         headers={
             "Content-Type": "application/x-www-form-urlencoded",
-        },
-        json={
-            "grant_type": 'authorization_code',
-            "code": code,
-            "redirect_uri": "https://app.sellscale.com/authcalendly",
+            "Accept": "application/json",
+            "Authorization": f"Basic {base64_string}",
         },
     )
     if res.status_code != 200:
@@ -49,14 +50,15 @@ def update_calendly_access_token_via_refresh_token(client_sdr_id: int, refresh_t
     
     sdr: ClientSDR = ClientSDR.query.get(client_sdr_id)
 
+    bytes_string = f'{os.environ.get("CALENDLY_CLIENT_ID")}:{os.environ.get("CALENDLY_CLIENT_SECRET")}'.encode('utf-8')
+    base64_string = base64.b64encode(bytes_string).decode('utf-8')
+
     res = requests.post(
-        url=f"https://auth.calendly.com/oauth/token",
+        url=f"https://auth.calendly.com/oauth/token?grant_type=refresh_token&refresh_token={refresh_token}",
         headers={
             "Content-Type": "application/x-www-form-urlencoded",
-        },
-        json={
-            "grant_type": 'refresh_token',
-            "refresh_token": refresh_token,
+            "Accept": "application/json",
+            "Authorization": f"Basic {base64_string}",
         },
     )
     if res.status_code != 200:
