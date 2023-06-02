4c09d87195f4b8b00217c8b859d52328e00470c9
Aaron Cassar <aaron@sellscale.com>
2023-06-02 16:57:43 -0700

    bump gen on convo update

--- src/li_conversation/services.py
+++ src/li_conversation/services.py
@@ -642,7 +642,7 @@ def scrape_conversation_queue():
             )
 
             # Generate a bump msg for the prospect
-            #generate_prospect_bump(prospect.client_sdr_id, prospect.id, prospect.li_conversation_urn_id)
+            generate_prospect_bump(prospect.client_sdr_id, prospect.id, prospect.li_conversation_urn_id)
 
             # Update calendar events
             populate_prospect_events(prospect.client_sdr_id, prospect.id)
