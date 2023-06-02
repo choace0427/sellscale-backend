d56c619a8b5124c32f1f2bec24e39c8ca92e9d46
Aaron Cassar <aaron@sellscale.com>
2023-06-02 16:55:03 -0700

    Added account research index list to log

--- src/message_generation/services.py
+++ src/message_generation/services.py
@@ -1843,7 +1843,7 @@ def generate_prospect_bump(client_sdr_id: int, prospect_id: int, convo_urn_id: s
     )
     research_str = ""
     send_slack_message(
-        message=f" - Account Research ({len(research_indexes)}):",
+        message=f" - Account Research ({len(research_indexes)}, {str(research_indexes)}):",
         webhook_urls=[URL_MAP["operations-auto-bump-msg-gen"]],
     )
     for i in research_indexes:
