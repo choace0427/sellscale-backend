de1512671fef48c58d3ba81d276f96e90c93c970
David Wei <david.wei4043@gmail.com>
2023-10-13 10:40:01 -0700

    Email Scheduling dict change

--- src/email_scheduling/models.py
+++ src/email_scheduling/models.py
@@ -2,6 +2,9 @@ from app import db
 
 import enum
 
+from src.email_outbound.models import ProspectEmail
+from src.prospecting.models import Prospect
+
 
 class EmailMessagingStatus(enum.Enum):
     NEEDS_GENERATION = "NEEDS_GENERATION"
@@ -49,10 +52,13 @@ class EmailMessagingSchedule(db.Model):
     nylas_thread_id = db.Column(db.String, nullable=True)
 
     def to_dict(self):
+        # Get the Prospect
+        prospect_email: ProspectEmail = ProspectEmail.query.filter_by(id=self.prospect_email_id).first()
+        prospect: Prospect = Prospect.query.filter_by(id=prospect_email.prospect_id).first()
+
         return {
             "id": self.id,
             "client_sdr_id": self.client_sdr_id,
-            "prospect_email_id": self.prospect_email_id,
             "email_type": self.email_type.value,
             "subject_line_id": self.subject_line_id,
             "body_id": self.body_id,
@@ -62,6 +68,8 @@ class EmailMessagingSchedule(db.Model):
             "send_status_error": self.send_status_error,
             "date_scheduled": self.date_scheduled,
             "nylas_message_id": self.nylas_message_id,
-            "nylas_thread_id": self.nylas_thread_id
+            "nylas_thread_id": self.nylas_thread_id,
+            "prospect_email": prospect_email.to_dict() if prospect_email else None,
+            "prospect": prospect.to_dict() if prospect else None
         }
 
