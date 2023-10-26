d1688c4ebfddab7a8d712ef4a693d8ef3b38301f
Aaron Cassar <aaron@sellscale.com>
2023-10-26 15:59:06 -0700

    Fixed int issue

--- src/prospecting/icp_score/services.py
+++ src/prospecting/icp_score/services.py
@@ -1434,8 +1434,11 @@ def update_icp_filters(client_archetype_id: int, filters):
             if value is ['None'] or value is [''] or value is []:
                 setattr(icp_scoring_ruleset, key, None)
             else:
-                setattr(icp_scoring_ruleset, key, [
+                if isinstance(value, list):
+                    setattr(icp_scoring_ruleset, key, [
                         s.replace('"', '') for s in value])
+                else:
+                    setattr(icp_scoring_ruleset, key, value)
 
         # Commit the changes to the database
         db.session.commit()
