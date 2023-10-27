0c9f69607c03d2243bb43cf3ed5eb5b6d89f159a
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-10-27 15:22:58 -0700

    Modify Date Scheduled (#1282)
    * Add modify_email_messaging_schedule_entry
    
    * Add test
    
    * Add controller patch_email_messaging_schedule_entry

--- tests/email_scheduling/test_email_scheduling_services.py
+++ tests/email_scheduling/test_email_scheduling_services.py
@@ -5,7 +5,7 @@ import mock
 import pytz
 from app import db, app
 from src.email_scheduling.models import EmailMessagingSchedule, EmailMessagingStatus, EmailMessagingType
-from src.email_scheduling.services import DEFAULT_SENDING_DELAY_INTERVAL, create_email_messaging_schedule_entry, generate_email_messaging_schedule_entry, get_initial_email_send_date, populate_email_messaging_schedule_entries, verify_followup_send_date
+from src.email_scheduling.services import DEFAULT_SENDING_DELAY_INTERVAL, create_email_messaging_schedule_entry, generate_email_messaging_schedule_entry, get_initial_email_send_date, modify_email_messaging_schedule_entry, populate_email_messaging_schedule_entries, verify_followup_send_date
 from src.message_generation.models import GeneratedMessage
 from src.prospecting.models import ProspectOverallStatus
 from test_utils import (
@@ -22,6 +22,103 @@ from test_utils import (
     basic_sdr_email_send_schedule
 )
 from decorators import use_app_context
+
+
+@use_app_context
+def test_modify_email_messaging_schedule_entry():
+    client = basic_client()
+    client_sdr = basic_client_sdr(client)
+    archetype = basic_archetype(client, client_sdr)
+    prospect = basic_prospect(client, archetype)
+    prospect_email = basic_prospect_email(prospect)
+    email_body_template = basic_email_sequence_step(
+        client_sdr=client_sdr,
+        client_archetype=archetype,
+        sequence_delay_days=3 # Monday send, Thursday next
+    )
+    email_subject_line_template = basic_email_subject_line_template(
+        client_sdr, archetype)
+    subject_line = basic_generated_message(prospect)
+    body = basic_generated_message(prospect)
+    email_bank = basic_sdr_email_bank(client_sdr)
+    schedule = basic_sdr_email_send_schedule(client_sdr, email_bank)
+
+    bump_accepted = basic_email_sequence_step(
+        client_sdr=client_sdr,
+        client_archetype=archetype,
+        overall_status=ProspectOverallStatus.ACCEPTED,
+        default=True,
+        sequence_delay_days=1 # Thursday send, Friday next
+    )
+    bump_1 = basic_email_sequence_step(
+        client_sdr=client_sdr,
+        client_archetype=archetype,
+        overall_status=ProspectOverallStatus.BUMPED,
+        bumped_count=1,
+        default=True,
+        sequence_delay_days=1 # Friday send, Monday next
+    )
+    bump_2 = basic_email_sequence_step(
+        client_sdr=client_sdr,
+        client_archetype=archetype,
+        overall_status=ProspectOverallStatus.BUMPED,
+        bumped_count=2,
+        default=True,
+    )
+
+    pst = pytz.timezone('America/Los_Angeles')
+    current_date = datetime.now(pst)
+    last_sunday = current_date.replace(year=2023, month=10, day=15, hour=9, minute=0, second=0, microsecond=0) # Last Sunday because we offset by 1 day
+    this_monday = current_date.replace(year=2023, month=10, day=16, hour=9, minute=0, second=0, microsecond=0)
+    this_thursday = current_date.replace(year=2023, month=10, day=19, hour=9, minute=0, second=0, microsecond=0)
+    this_friday = current_date.replace(year=2023, month=10, day=20, hour=9, minute=0, second=0, microsecond=0)
+    next_monday = current_date.replace(year=2023, month=10, day=23, hour=9, minute=0, second=0, microsecond=0)
+
+    this_wednesday = current_date.replace(year=2023, month=10, day=18, hour=9, minute=0, second=0, microsecond=0)
+
+    with freeze_time(last_sunday):
+        assert EmailMessagingSchedule.query.count() == 0
+        ids = populate_email_messaging_schedule_entries(
+            client_sdr_id=client_sdr.id,
+            prospect_email_id=prospect_email.id,
+            subject_line_id=subject_line.id,
+            body_id=body.id,
+            initial_email_subject_line_template_id=email_subject_line_template.id,
+            initial_email_body_template_id=email_body_template.id,
+        )
+        assert EmailMessagingSchedule.query.count() == 4
+        assert len(ids) == 4
+        assert EmailMessagingSchedule.query.get(ids[0]).email_type == EmailMessagingType.INITIAL_EMAIL
+        assert EmailMessagingSchedule.query.get(ids[0]).date_scheduled.date() == this_monday.date()
+        assert EmailMessagingSchedule.query.get(ids[1]).email_type == EmailMessagingType.FOLLOW_UP_EMAIL
+        assert EmailMessagingSchedule.query.get(ids[1]).date_scheduled.date() == this_thursday.date()
+        assert EmailMessagingSchedule.query.get(ids[2]).email_type == EmailMessagingType.FOLLOW_UP_EMAIL
+        assert EmailMessagingSchedule.query.get(ids[2]).date_scheduled.date() == this_friday.date()
+        assert EmailMessagingSchedule.query.get(ids[3]).email_type == EmailMessagingType.FOLLOW_UP_EMAIL
+        assert EmailMessagingSchedule.query.get(ids[3]).date_scheduled.date() == next_monday.date()
+
+        success, _ = modify_email_messaging_schedule_entry(
+            email_messaging_schedule_id=ids[1],
+            date_scheduled=this_wednesday
+        )
+        assert success
+        assert EmailMessagingSchedule.query.get(ids[1]).date_scheduled.date() == this_wednesday.date()
+        assert EmailMessagingSchedule.query.get(ids[2]).date_scheduled.date() == this_thursday.date()
+        assert EmailMessagingSchedule.query.get(ids[3]).date_scheduled.date() == this_friday.date()
+
+        success, reason = modify_email_messaging_schedule_entry(
+            email_messaging_schedule_id=ids[1],
+            date_scheduled=last_sunday - timedelta(days=1)
+        )
+        assert not success
+        assert reason == "Cannot reschedule an email in the past"
+
+        success, reason = modify_email_messaging_schedule_entry(
+            email_messaging_schedule_id=ids[1],
+            date_scheduled=this_monday - timedelta(days=1)
+        )
+        assert not success
+        assert reason == "Cannot reschedule an email to occur before the previous email in the sequence"
 
 
 @use_app_context
