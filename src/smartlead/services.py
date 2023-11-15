2fdb8133a3d52a65bce0f8ec6fce5549b2870332
Aaron Cassar <aaron@sellscale.com>
2023-11-15 14:59:08 -0800

    Updated email warming system

--- src/smartlead/services.py
+++ src/smartlead/services.py
@@ -7,7 +7,6 @@ from src.client.models import ClientSDR
 from src.smartlead.smartlead import Smartlead, EmailWarming
 
 from app import db, celery
-from src.research.linkedin.services import get_research_and_bullet_points_new
 
 def get_all_email_warmings() -> list[EmailWarming]:
   
@@ -16,6 +15,8 @@ def get_all_email_warmings() -> list[EmailWarming]:
   
   warmings = []
   for email in emails:
+    
+    warmup_stats = sl.get_warmup_stats(email.get("id"))
     warming = EmailWarming(
       id=email.get("id"),
       name=email.get("from_name"),
@@ -24,6 +25,13 @@ def get_all_email_warmings() -> list[EmailWarming]:
       total_sent=email.get("warmup_details", {}).get("total_sent_count"),
       total_spam=email.get("warmup_details", {}).get("total_spam_count"),
       warmup_reputation=email.get("warmup_details", {}).get("warmup_reputation"),
+      
+      sent_count=warmup_stats.get("sent_count"),
+      spam_count=warmup_stats.get("spam_count"),
+      inbox_count=warmup_stats.get("inbox_count"),
+      warmup_email_received_count=warmup_stats.get("warmup_email_received_count"),
+      stats_by_date=warmup_stats.get("stats_by_date"),
+      percent_complete=get_warmup_percentage(warmup_stats.get("stats_by_date"))
     )
     warmings.append(warming)
   
@@ -48,7 +56,7 @@ def get_email_warmings_for_sdr(client_sdr_id: int) -> list[EmailWarming]:
       if warming.id == old_warming.get("id"):
         
         # Finished warming
-        if warming.warmup_reputation == '100%' and old_warming.get("warmup_reputation") != '100%':
+        if warming.percent_complete == 100 and old_warming.get('percent_complete') != 100:
           send_slack_message(
               message="🔥 Domain warmed",
               blocks=[
@@ -77,4 +85,20 @@ def get_email_warmings_for_sdr(client_sdr_id: int) -> list[EmailWarming]:
   db.session.commit()
       
   return warmings
+  
+
+def get_warmup_percentage(warming_stats_by_date: list) -> int:
+    WARMUP_LENGTH = 14
+  
+    first = warming_stats_by_date[0]
+    last = warming_stats_by_date[-1]
+    
+    # Convert date strings to datetime objects
+    date_first = datetime.datetime.strptime(first.get('date'), '%Y-%m-%d')
+    date_last = datetime.datetime.strptime(last.get('date'), '%Y-%m-%d')
+
+    # Calculate the difference between the two dates
+    date_difference = date_last - date_first
+
+    return min(round((date_difference.days / WARMUP_LENGTH) * 100, 0), 100)
   
--- src/smartlead/smartlead.py
+++ src/smartlead/smartlead.py
@@ -33,6 +33,11 @@ class Smartlead:
         url = f"{self.BASE_URL}/campaigns/{campaign_id}/leads?api_key={self.api_key}&offset={offset}&limit={limit}"
         response = requests.get(url)
         return response.json()
+      
+    def get_warmup_stats(self, email_account_id):
+        url = f"{self.BASE_URL}/email-accounts/{email_account_id}/warmup-stats?api_key={self.api_key}"
+        response = requests.get(url)
+        return response.json()
     
     def post_campaign_leads(self, campaign_id, lead_list):
         """`lead_list` format is 
@@ -78,7 +83,7 @@ class Smartlead:
 
 
 class EmailWarming:
-    def __init__(self, id: int, name: str, email: str, status: str, total_sent: int, total_spam: int, warmup_reputation: str):
+    def __init__(self, id: int, name: str, email: str, status: str, total_sent: int, total_spam: int, warmup_reputation: str, sent_count: int, spam_count: int, inbox_count: int, warmup_email_received_count: int, stats_by_date: list, percent_complete: int):
         self.id = id
         self.name = name
         self.email = email
@@ -86,6 +91,12 @@ class EmailWarming:
         self.total_sent = total_sent
         self.total_spam = total_spam
         self.warmup_reputation = warmup_reputation
+        self.sent_count = sent_count
+        self.spam_count = spam_count
+        self.inbox_count = inbox_count
+        self.warmup_email_received_count = warmup_email_received_count
+        self.stats_by_date = stats_by_date
+        self.percent_complete = percent_complete
         
     def to_dict(self):
         return {
@@ -95,7 +106,13 @@ class EmailWarming:
             "status": self.status,
             "total_sent": self.total_sent,
             "total_spam": self.total_spam,
-            "warmup_reputation": self.warmup_reputation
+            "warmup_reputation": self.warmup_reputation,
+            "sent_count": self.sent_count,
+            "spam_count": self.spam_count,
+            "inbox_count": self.inbox_count,
+            "warmup_email_received_count": self.warmup_email_received_count,
+            "stats_by_date": self.stats_by_date,
+            "percent_complete": self.percent_complete
         }
 
 
