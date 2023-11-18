7726c4a28b7c56769ee6b244628d87f901612645
Aakash Adesara <aakash.adesara@gmail.com>
2023-11-17 17:27:43 -0800

    Fix Override Campaign Mapping (#1375)

--- src/smartlead/controllers.py
+++ src/smartlead/controllers.py
@@ -1,6 +1,10 @@
 from typing import List
 from flask import Blueprint, request, jsonify
-from src.smartlead.services import get_email_warmings_for_sdr, set_campaign_id, sync_prospects_to_campaign
+from src.smartlead.services import (
+    get_email_warmings_for_sdr,
+    set_campaign_id,
+    sync_prospects_to_campaign,
+)
 from app import db
 import os
 from src.authentication.decorators import require_user
@@ -8,19 +12,26 @@ from src.utils.request_helpers import get_request_parameter
 
 SMARTLEAD_BLUEPRINT = Blueprint("smart_email", __name__)
 
+
 @SMARTLEAD_BLUEPRINT.route("/email_warmings", methods=["GET"])
 @require_user
 def get_email_warmings(client_sdr_id: int):
-
     email_warmings = get_email_warmings_for_sdr(client_sdr_id)
 
-    return jsonify({"message": "Success", "data": [warming.to_dict() for warming in email_warmings]}), 200
+    return (
+        jsonify(
+            {
+                "message": "Success",
+                "data": [warming.to_dict() for warming in email_warmings],
+            }
+        ),
+        200,
+    )
 
 
 @SMARTLEAD_BLUEPRINT.route("/set_campaign", methods=["POST"])
 @require_user
 def post_set_campaign(client_sdr_id: int):
-  
     archetype_id = get_request_parameter(
         "archetype_id", request, json=True, required=True, parameter_type=int
     )
@@ -30,21 +41,19 @@ def post_set_campaign(client_sdr_id: int):
 
     success = set_campaign_id(archetype_id, campaign_id)
 
-    return jsonify({"message": "Success", "data": success }), 200
-  
-  
+    return jsonify({"message": "Success", "data": success}), 200
+
+
 @SMARTLEAD_BLUEPRINT.route("/sync_prospects_to_campaign", methods=["POST"])
 @require_user
-def post_set_campaign(client_sdr_id: int):
-  
+def post_set_client_sdr_id(client_sdr_id: int):
     archetype_id = get_request_parameter(
         "archetype_id", request, json=True, required=True, parameter_type=int
     )
 
     success, amount = sync_prospects_to_campaign(client_sdr_id, archetype_id)
 
-    return jsonify({"message": "Success", "data": {
-      "success": success,
-      "amount": amount
-    }}), 200
-  
+    return (
+        jsonify({"message": "Success", "data": {"success": success, "amount": amount}}),
+        200,
+    )
