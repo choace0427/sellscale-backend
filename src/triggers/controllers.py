6349b402c56c34356cc2b5b9c80f255240b44406
Aakash Adesara <aakash.adesara@gmail.com>
2023-11-20 11:21:04 -0800

    Added Client SDR ID to Migrations Trigger (#1379)

--- src/triggers/controllers.py
+++ src/triggers/controllers.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from src.authentication.decorators import require_user
 from src.triggers.models import Trigger
 
 TRIGGERS_BLUEPRINT = Blueprint("triggers", __name__)
@@ -7,3 +8,10 @@ TRIGGERS_BLUEPRINT = Blueprint("triggers", __name__)
 @TRIGGERS_BLUEPRINT.route("/")
 def index():
     return "OK", 200
+
+
+@TRIGGERS_BLUEPRINT.route("/all", methods=["GET"])
+@require_user
+def get_triggers(client_sdr_id: int):
+    triggers: list = Trigger.query.filter_by(client_sdr_id=client_sdr_id).all()
+    return {"triggers": [trigger.to_dict() for trigger in triggers]}, 200
--- src/triggers/models.py
+++ src/triggers/models.py
@@ -1,6 +1,8 @@
 from app import db
 from enum import Enum
 
+from src.client.models import ClientArchetype
+
 
 class TriggerType(Enum):
     RECURRING_PROSPECT_SCRAPE = "recurring_prospect_scrape"
@@ -23,7 +25,34 @@ class Trigger(db.Model):
     trigger_type = db.Column(db.Enum(TriggerType), nullable=False)
     trigger_config = db.Column(db.JSON, nullable=False, default={})
 
+    client_sdr_id = db.Column(
+        db.Integer, db.ForeignKey("client_sdr.id"), nullable=False
+    )
     client_archetype_id = db.Column(
         db.Integer, db.ForeignKey("client_archetype.id"), nullable=True
     )
     active = db.Column(db.Boolean, nullable=False, default=True)
+
+    def to_dict(self, include_rich_info: bool = False):
+        retval = {
+            "id": self.id,
+            "emoji": self.emoji,
+            "name": self.name,
+            "description": self.description,
+            "last_run": self.last_run,
+            "next_run": self.next_run,
+            "interval_in_minutes": self.interval_in_minutes,
+            "trigger_type": self.trigger_type.value,
+            "trigger_config": self.trigger_config,
+            "client_archetype_id": self.client_archetype_id,
+            "active": self.active,
+        }
+
+        if include_rich_info:
+            archetype: ClientArchetype = ClientArchetype.query.get(
+                self.client_archetype_id
+            )
+            if archetype:
+                retval["client_archetype"] = archetype.to_dict()
+
+        return retval
