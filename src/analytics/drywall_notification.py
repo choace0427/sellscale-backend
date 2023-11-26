70c0fcadf9f936d834af6ff7ac2208de3c9032b9
Aakash Adesara <aakash.adesara@gmail.com>
2023-11-26 11:51:58 -0800

    Drywall Changed to 48 Hours (#1405)

--- src/analytics/drywall_notification.py
+++ src/analytics/drywall_notification.py
@@ -10,7 +10,7 @@ def notify_clients_with_no_updates():
     with d as (
         select 
             client.company,
-            count(distinct prospect.id) filter (where prospect_status_records.to_status in ('ACCEPTED', 'ACTIVE_CONVO', 'DEMO_SET') and prospect_status_records.created_at > NOW() - '24 hours'::INTERVAL) "# Notifications in 24 Hours",
+            count(distinct prospect.id) filter (where prospect_status_records.to_status in ('ACCEPTED', 'ACTIVE_CONVO', 'DEMO_SET') and prospect_status_records.created_at > NOW() - '48 hours'::INTERVAL) "# Notifications in 48 Hours",
             max(prospect_status_records.created_at) filter (where prospect_status_records.to_status  in ('ACCEPTED', 'ACTIVE_CONVO', 'DEMO_SET')) "Latest Notification"
         from client
             join client_sdr on client_sdr.client_id = client.id
@@ -27,7 +27,7 @@ def notify_clients_with_no_updates():
             ' hours ago'
         ) "Days Since Last Ping"
     from d
-    where d."# Notifications in 24 Hours" = 0;
+    where d."# Notifications in 48 Hours" = 0;
     """
 
     # Execute Query
@@ -36,10 +36,10 @@ def notify_clients_with_no_updates():
 
     # Check if there are clients to notify
     if not clients:
-        return "No clients with no updates in the last 24 hours."
+        return "No clients with no updates in the last 48 hours."
 
     message_header = (
-        "*Attention: These clients have had no updates in the last 24 hours on: "
+        "*Attention: These clients have had no updates in the last 48 hours on: "
         + datetime.datetime.now().strftime("%m/%d/%Y")
         + "*"
     )
