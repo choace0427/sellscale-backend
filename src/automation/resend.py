960ffaa9c5f3ee145b38a77508f8fb490094d5df
Aakash Adesara <aakash.adesara@gmail.com>
2023-12-01 14:14:47 -0800

    Weekly Report Updates 2 (#1433)

--- src/automation/resend.py
+++ src/automation/resend.py
@@ -5,12 +5,20 @@ import resend
 resend.api_key = "re_bFWa8smr_9pFd7krp6MnnUAYmvF1KYDDv"
 
 
-def send_email(html: str, title: str, to_emails: list[str] = []):
+def send_email(
+    html: str,
+    title: str,
+    to_emails: list[str] = [],
+    cc_emails: list[str] = [],
+    bcc_emails: list[str] = [],
+):
     params = {
         "from": "ai@sellscale.com",
         "to": to_emails,
         "subject": title,
         "html": html,
+        "cc": cc_emails,
+        "bcc": bcc_emails,
     }
 
     email = resend.Emails.send(params)
