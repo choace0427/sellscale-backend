78aebc16be53c58d5fbef4b254db3174dd3303ce
Aakash Adesara <aakash.adesara@gmail.com>
2024-01-08 15:05:37 -0800

    Search Segment by Service Handler WIP (#1664)

--- src/segment/services.py
+++ src/segment/services.py
@@ -1,5 +1,6 @@
 from app import db
 from sqlalchemy.orm import attributes
+from src.client.models import ClientArchetype, ClientSDR
 from src.prospecting.models import Prospect
 from src.segment.models import Segment
 
@@ -80,3 +81,75 @@ def add_prospects_to_segment(prospect_ids: list[int], new_segment_id: int):
     db.session.commit()
 
     return True, "Prospects added to segment"
+
+
+def find_prospects_by_segment_filters(
+    client_sdr_id: int,
+    included_title_keywords: list[str] = [],
+    excluded_title_keywords: list[str] = [],
+    included_seniority_keywords: list[str] = [],
+    excluded_seniority_keywords: list[str] = [],
+    included_company_keywords: list[str] = [],
+    excluded_company_keywords: list[str] = [],
+) -> list[dict]:
+    base_query = (
+        Prospect.query.join(ClientArchetype)
+        .join(ClientSDR)
+        .filter(ClientSDR.id == client_sdr_id)
+    )
+
+    if included_title_keywords:
+        for keyword in included_title_keywords:
+            base_query = base_query.filter(Prospect.title.ilike(f"%{keyword}%"))
+
+    if excluded_title_keywords:
+        for keyword in excluded_title_keywords:
+            base_query = base_query.filter(~Prospect.title.ilike(f"%{keyword}%"))
+
+    if included_seniority_keywords:
+        for keyword in included_seniority_keywords:
+            base_query = base_query.filter(Prospect.title.ilike(f"%{keyword}%"))
+
+    if excluded_seniority_keywords:
+        for keyword in excluded_seniority_keywords:
+            base_query = base_query.filter(~Prospect.title.ilike(f"%{keyword}%"))
+
+    if included_company_keywords:
+        for keyword in included_company_keywords:
+            base_query = base_query.filter(Prospect.company.ilike(f"%{keyword}%"))
+
+    if excluded_company_keywords:
+        for keyword in excluded_company_keywords:
+            base_query = base_query.filter(~Prospect.company.ilike(f"%{keyword}%"))
+
+    # extract just the columns I need:
+    #             prospect.id,
+    #     prospect.full_name "Name",
+    #   prospect.title "Title",
+    #   prospect.company "Company",
+    #   client_archetype.archetype "Campaign",
+    #   case
+    #     when segment.segment_title is null then 'Uncategorized'
+    #     else segment.segment_title
+    #   end "Segment"
+
+    prospects = base_query.with_entities(
+        Prospect.id,
+        Prospect.full_name,
+        Prospect.title,
+        Prospect.company,
+        ClientArchetype.archetype,
+        Segment.segment_title,
+    ).all()
+
+    return [
+        {
+            "id": prospect.id,
+            "name": prospect.full_name,
+            "title": prospect.title,
+            "company": prospect.company,
+            "campaign": prospect.archetype,
+            "segment": prospect.segment_title,
+        }
+        for prospect in prospects
+    ]
