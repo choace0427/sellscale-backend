38eead66816e0c5fe4b892b3452b410b8cf7c171
Quzzar <31683740+Quzzar@users.noreply.github.com>
2024-01-19 14:08:33 -0800

    Aaron 01 19 24 inbox schleps (#1758)
    * Fixed bug
    
    * Added valid to other dicts
    
    * Added inbox improvements
    
    * Fixed bug
    
    * Added img_url
    
    ---------
    
    Co-authored-by: Aaron Cassar <aaron@sellscale.com>

--- src/prospecting/services.py
+++ src/prospecting/services.py
@@ -3673,6 +3673,7 @@ def inbox_restructure_fetch_prospects(client_sdr_id: int):
       prospect.hidden_until,
       prospect.icp_fit_score,
       prospect.overall_status,
+      prospect.img_url,
       case
         when prospect_email.created_at > generated_message.created_at or generated_message.created_at is null
           then 'EMAIL'
