f68a13464bec63f84a26b72bab323ef86d8b641b
Aakash Adesara <aakash.adesara@gmail.com>
2024-01-20 16:53:37 -0800

    Find Companies from Track Events (#1771)

--- src/track/services.py
+++ src/track/services.py
@@ -1,3 +1,7 @@
+import requests
+from sqlalchemy import or_
+from src.company.models import Company
+from src.prospecting.models import Prospect
 from src.track.models import TrackEvent, TrackSource
 from app import db
 from src.utils.slack import URL_MAP, send_slack_message
@@ -35,4 +39,38 @@ def create_track_event(
         webhook_urls=[URL_MAP["eng-sandbox"]],
     )
 
+    find_company(track_event.id)
+
     return True
+
+
+def find_company(track_event_id):
+    # Step 1: Find the track event
+    track_event = TrackEvent.query.get(track_event_id)
+    if track_event is None:
+        return "Track event not found"
+
+    # Step 2: Use the track_event.ip_address to find the payload
+    url = f"https://api.orginfo.io/data/v1/org/company?key=75a60f3adfc391227bd9e5cfb8d266bead61a02f&ip={track_event.ip_address}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return "Failed to retrieve data from API"
+
+    response_data = response.json()
+    company_website = response_data.get("data", {}).get("website")
+    if not company_website:
+        return "Company website not found in API response"
+
+    # Step 3: Find the company with a matching career page URL
+    prospect = Prospect.query.filter(
+        Prospect.company_url.ilike(f"%{company_website}%"),
+        Prospect.company_id != None,
+    ).first()
+    if prospect is None:
+        return "Company with url: {} not found".format(company_website)
+
+    # Step 4: Update track_event with the company's ID
+    track_event.company_id = prospect.company_id
+    db.session.commit()
+
+    return f"Track event updated with company ID: {prospect.company_id}"
