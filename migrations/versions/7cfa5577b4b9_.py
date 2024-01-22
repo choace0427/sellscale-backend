3442fd9575fffb9cc22722838eb5b37ecd4ef860
David Wei <49255196+davidmwei@users.noreply.github.com>
2024-01-22 13:52:22 -0800

    HOTFIX: Merge Heads (#1779)

---
+++ migrations/versions/7cfa5577b4b9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7cfa5577b4b9
+Revises: 5ab8cff42cea, 785137c285bb
+Create Date: 2024-01-22 15:51:00.917590
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7cfa5577b4b9"
+down_revision = ("5ab8cff42cea", "785137c285bb")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
