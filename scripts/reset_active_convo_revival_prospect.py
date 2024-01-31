e87319e3942b6b3f168bf8b47e502a9d0c4ed2aa
Aakash Adesara <aakash.adesara@gmail.com>
2024-01-31 15:22:56 -0800

    Backfill Incorrect Active Convo Revival Prospects (#1899)

---
+++ scripts/reset_active_convo_revival_prospect.py
@@ -0,0 +1,39 @@
+from app import db
+from model_import import (
+    Prospect,
+    ProspectStatusRecords,
+    ProspectOverallStatus,
+    ProspectStatus,
+)
+from src.message_generation.models import GeneratedMessageAutoBump
+
+exec(
+    """
+def reset_active_convo_revival_prospect(prospect_id: int):
+    prospect: Prospect = Prospect.get_by_id(prospect_id)
+    if prospect:
+        prospect_status_records = ProspectStatusRecords.query.filter(
+            ProspectStatusRecords.prospect_id == prospect_id,
+            ProspectStatusRecords.to_status.in_(
+                [ProspectStatus.ACTIVE_CONVO_REVIVAL, ProspectStatus.ACTIVE_CONVO]
+            ),
+        )
+
+        for record in prospect_status_records:
+            db.session.delete(record)
+
+        prospect.overall_status = ProspectOverallStatus.BUMPED
+        prospect.status = ProspectStatus.RESPONDED
+        db.session.add(prospect)
+
+        auto_generated_messages: list[GeneratedMessageAutoBump] = GeneratedMessageAutoBump.query.filter(
+            GeneratedMessageAutoBump.prospect_id == prospect_id
+        ).all()
+        for message in auto_generated_messages:
+            db.session.delete(message)
+
+        db.session.commit()
+
+        return True
+"""
+)
