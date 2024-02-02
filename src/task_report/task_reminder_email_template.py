dfc8ff6df79877513137c05bb12ba64adeeed857
Aakash Adesara <aakash.adesara@gmail.com>
2024-02-02 09:39:31 -0800

    Task Report Schleps (#1913)

--- src/task_report/task_reminder_email_template.py
+++ src/task_report/task_reminder_email_template.py
@@ -20,13 +20,8 @@ START_BLOCK = """
                     <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="justify-content:space-between;background-color:black;padding:15px 40px 15px 40px;border-radius:6px;width:100%">
                       <tbody style="width:100%">
                         <tr style="width:100%">
-                          <td data-id="__react-email-column" style="text-align:start"><img src="https://www.aitoolsclub.com/content/images/2023/06/Screenshot-2023-06-28-151838.png" width="150px" style="background:black" /></td>
-                          <td data-id="__react-email-column" style="text-align:end"><a href="{task_link}" style="color:gray;text-decoration:none" target="_blank">Go to Dashboard<svg style="margin-left:7px;width:18px;height:18px;color:gray" width="24" height="24" viewBox="0 0 24 18" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z"></path>
-                                <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
-                                <line x1="10" y1="14" x2="20" y2="4"></line>
-                                <polyline points="15 4 20 4 20 9"></polyline>
-                              </svg></a></td>
+                          <td data-id="__react-email-column" style="text-align:center"><img src="https://www.aitoolsclub.com/content/images/2023/06/Screenshot-2023-06-28-151838.png" width="150px" style="background:black" /></td>
+                          
                         </tr>
                       </tbody>
                     </table>
@@ -34,7 +29,7 @@ START_BLOCK = """
                       <tbody>
                         <tr>
                           <td>
-                            <p style="font-size:16px;line-height:24px;margin:16px 0;color:gray;font-weight:500;text-align:start">You have <b>{num_tasks} new tasks</b> to complete. Sign in to SellScale to view and complete them.</p>
+                            <p style="font-size:16px;line-height:24px;margin:16px 0;color:gray;font-weight:500;text-align:start">You have <b>{num_tasks} new tasks</b> to complete.</p>
                             <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-bottom:20px">
                             <tbody style="width:100%">
 """
@@ -51,7 +46,7 @@ TASK_BLOCK = """
                                         <p style="font-size:20px;line-height:24px;margin:16px 0; margin-bottom: 0px; font-weight:600">{emoji} {title}</p>
                                         <p style="font-size:14px;line-height:24px;margin:16px 0; margin-top: 2px; color:gray;font-weight:500;margin-top:-10px">{description}</p>
                                         <a href="{task_url}" style="margin-bottom:22px;background-color:#fd4efe;color:white;border-radius:6px;padding:10px 20px 10px 20px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%;mso-text-raise:15" hidden>&nbsp;</i><![endif]--></span>
-                                        <span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:7.5px"><div style="display:flex;align-items:center"><p style="font-size:14px;line-height:24px;margin:0px">{cta}</p></span></a>
+                                        <span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:7.5px"><div style="display:flex;align-items:center"><p style="font-size:14px;line-height:24px;margin:0px">Let's do it</p></span></a>
                                       </td>
                                     </tr>
                                   </tbody>
@@ -82,8 +77,7 @@ FOOTER_BLOCK = """
                             <tr>
                               <td>
                                 <div style="border:2px solid #edebef;border-style:dashed;border-radius:8px;padding:20px 40px">
-                                  <p style="font-size:16px;line-height:24px;margin:16px 0;text-align:start"><b>⚠️ Important ⚠️: </b>Do not forward this email to others as they will be able to log in to your SellScale account</p>
-                                  <div style="display:flex;justify-content:space-between;margin-block:16px"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0b5lrIs317CdpSkXImMEzyw8FwGX5ogdiKTT4xwGb&amp;s" width="150px" style="background:black" /></div>
+                                  <p style="font-size:16px;line-height:24px;margin:16px 0;text-align:center; color: gray;"><b>⚠️ Important ⚠️: </b>Do not forward this email to others as they will be able to log in to your SellScale account</p>
                                 </div>
                               </td>
                             </tr>
@@ -130,7 +124,6 @@ def generate_task_report_html(client_sdr_id: int):
             emoji=entry.emoji,
             title=entry.title,
             description=entry.subtitle,
-            cta=entry.cta,
             task_url="https://app.sellscale.com/authenticate?stytch_token_type=direct&token="
             + auth_token
             + "&redirect=task/"
