fe49903ff4d6e1acfc4870ff0455a7618cf26c4e
Aakash Adesara <aakash.adesara@gmail.com>
2024-02-21 13:48:14 -0800

    Merge Heads to Fix Migration Problem (#2082)

---
+++ migrations/versions/34aad0779519_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 34aad0779519
+Revises: 2aeb6f172bc1, a4717112e41e
+Create Date: 2024-02-21 13:46:42.577675
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "34aad0779519"
+down_revision = ("2aeb6f172bc1", "a4717112e41e")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
