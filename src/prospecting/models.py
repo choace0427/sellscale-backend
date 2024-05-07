bf63a262eb75958cdedf4cabe669ddb01696f635
Aakash Adesara <aakash.adesara@gmail.com>
2024-05-07 14:47:39 -0700

    Added 'is_champion' to Prospect Model (#2446)

--- src/prospecting/models.py
+++ src/prospecting/models.py
@@ -524,6 +524,8 @@ class Prospect(db.Model):
 
     smartlead_campaign_id = db.Column(db.Integer, nullable=True)
 
+    is_champion = db.Column(db.Boolean, nullable=True)
+
     __table_args__ = (db.Index("idx_li_urn_id", "li_urn_id"),)
 
     def regenerate_uuid(self) -> str:
@@ -600,6 +602,7 @@ class Prospect(db.Model):
             "merge_contact_id": self.merge_contact_id,
             "merge_opportunity_id": self.merge_opportunity_id,
             "merge_lead_id": self.merge_lead_id,
+            "is_champion": self.is_champion,
         }
 
     def to_dict(
