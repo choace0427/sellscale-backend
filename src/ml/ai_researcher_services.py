a9f72ee040666b6240caf0793cc44e2f61111dae
Aakash Adesara <aakash.adesara@gmail.com>
2024-06-02 11:45:18 -0700

    Run AI Personalizer on Email (#2563)

--- src/ml/ai_researcher_services.py
+++ src/ml/ai_researcher_services.py
@@ -398,76 +398,84 @@ def get_generated_email(email_body, prospectId):
     return answer
 
 def run_ai_personalizer_on_prospect_email(prospect_email_id: int):
-    prospect_email: ProspectEmail = ProspectEmail.query.get(prospect_email_id)
-    prospect_id: int = prospect_email.prospect_id
-    prospect: Prospect = Prospect.query.get(prospect_id)
+    try:
+        prospect_email: ProspectEmail = ProspectEmail.query.get(prospect_email_id)
+        prospect_id: int = prospect_email.prospect_id
+        prospect: Prospect = Prospect.query.get(prospect_id)
 
-    run_all_ai_researcher_questions_for_prospect(
-        client_sdr_id=prospect.client_sdr_id,
-        prospect_id=prospect_id
-    )
+        run_all_ai_researcher_questions_for_prospect(
+            client_sdr_id=prospect.client_sdr_id,
+            prospect_id=prospect_id
+        )
 
-    email_body_id = prospect_email.personalized_body
-    generated_message: GeneratedMessage = GeneratedMessage.query.get(email_body_id)
-    origina_email_body = generated_message.completion
+        email_body_id = prospect_email.personalized_body
+        generated_message: GeneratedMessage = GeneratedMessage.query.get(email_body_id)
+        origina_email_body = generated_message.completion
 
-    positive_research_points = AIResearcherAnswer.query.filter_by(prospect_id=prospect_id, is_yes_response=True).all()
+        positive_research_points = AIResearcherAnswer.query.filter_by(prospect_id=prospect_id, is_yes_response=True).all()
 
-    personalizations = ""
-    for point in positive_research_points:
-        point: AIResearcherAnswer = point
-        question: AIResearcherQuestion = AIResearcherQuestion.query.get(point.question_id)
+        personalizations = ""
+        for point in positive_research_points:
+            point: AIResearcherAnswer = point
+            question: AIResearcherQuestion = AIResearcherQuestion.query.get(point.question_id)
 
-        raw_data = point.raw_response
-        relevancy_reason = question.relevancy
-        relevancy_explanation = point.relevancy_explanation
+            raw_data = point.raw_response
+            relevancy_reason = question.relevancy
+            relevancy_explanation = point.relevancy_explanation
 
-        personalizations += f"Raw Data: {raw_data}\nQuestion: {relevancy_reason}\nRelevancy Explanation: {relevancy_explanation}\n\n"
+            personalizations += f"Raw Data: {raw_data}\nQuestion: {relevancy_reason}\nRelevancy Explanation: {relevancy_explanation}\n\n"
 
-    prompt = """
-You are an emailer personalizer. Combine the sequence provided with the personalization to create a personalized email. Keep it as short as possible. Feel free to spread the personalizations across the email to keep length minimal. Try to include personalization at the beginning since it helps with open rates.
+        prompt = """
+    You are an emailer personalizer. Combine the sequence provided with the personalization to create a personalized email. Keep it as short as possible. Feel free to spread the personalizations across the email to keep length minimal. Try to include personalization at the beginning since it helps with open rates.
 
-Original Email:
-{original_email_body}
+    Original Email:
+    {original_email_body}
 
-### Personalization
-{personalizations}
+    ### Personalization
+    {personalizations}
 
-Prospect Information:
-- name: {name}
-- title: {title}
-- company: {company}
+    Prospect Information:
+    - name: {name}
+    - title: {title}
+    - company: {company}
 
-Tie in relevant details into the emails so it is compelling for the person I am reaching out to.
+    Tie in relevant details into the emails so it is compelling for the person I am reaching out to.
 
-Example
-Hi Dr Xyz..
+    Example
+    Hi Dr Xyz..
 
-Tie in relevant details into the emails so it is compelling for the person I am reaching out to.
-NOTE: Try not to increase the length of the email - seamlessly incorproate personalization to make it same or shorter length.
-NOTE: Include the personalization at the beginning since that makes for a better hook and helps open rates.
-NOTE: Only respond with the personalized email, nothing else.
-NOTE: When adding personalization, ensure that you write in a natural way that is not robotic or forced. Additionally, ensure you tie in the personalization in a way that is relevant to the email content.
+    Tie in relevant details into the emails so it is compelling for the person I am reaching out to.
+    NOTE: Try not to increase the length of the email - seamlessly incorproate personalization to make it same or shorter length.
+    NOTE: Include the personalization at the beginning since that makes for a better hook and helps open rates.
+    NOTE: Only respond with the personalized email, nothing else.
+    NOTE: When adding personalization, ensure that you write in a natural way that is not robotic or forced. Additionally, ensure you tie in the personalization in a way that is relevant to the email content.
 
-Personalized email:"""
+    Personalized email:"""
 
-    prompt = prompt.format(
-        original_email_body=origina_email_body,
-        personalizations=personalizations,
-        name=prospect.first_name,
-        title=prospect.title,
-        company=prospect.company
-    )
+        prompt = prompt.format(
+            original_email_body=origina_email_body,
+            personalizations=personalizations,
+            name=prospect.first_name,
+            title=prospect.title,
+            company=prospect.company
+        )
 
-    answer = wrapped_chat_gpt_completion(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        model='gpt-4o',
-        max_tokens=1000
-    )
+        answer = wrapped_chat_gpt_completion(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            model='gpt-4o',
+            max_tokens=1000
+        )
 
-    return prompt, answer
+        prospect_email.personalized_body = answer
+        db.session.add(prospect_email)
+        db.session.commit()
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
