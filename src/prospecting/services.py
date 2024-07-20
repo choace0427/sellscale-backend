041d359c9ececfef9927e7ad7eb819237b2025d1
Khoa Le <77949166+NEUKhoaLe@users.noreply.github.com>
2024-07-19 21:04:52 -0700

    fixed small bug (#2809)
    Co-authored-by: Khoa Le <khoa@sellscale.com>

--- src/prospecting/services.py
+++ src/prospecting/services.py
@@ -323,7 +323,7 @@ def get_prospect_duplicate_details(
             "sdr": result.name,
             "archetype": result.archetype,
             "status": result.status,
-            "segment_title": result.status
+            "segment_title": result.segment_title,
         }
 
 
