8c6c473cb52150bb4e16e6b7a7d2e6b75d064c39
Khoa Le <77949166+NEUKhoaLe@users.noreply.github.com>
2024-07-30 18:51:09 -0700

    Added count column to internal default voices (#2880)
    * Added count column to internal default voices
    
    * Added count column to internal default voices and make nullable
    
    ---------
    
    Co-authored-by: Khoa Le <khoa@sellscale.com>

--- src/internal_default_voices/models.py
+++ src/internal_default_voices/models.py
@@ -8,10 +8,14 @@ class InternalDefaultVoices(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(4000), nullable=True)
+    count_ctas = db.Column(db.Integer, nullable=True)
+    count_bumps = db.Column(db.Integer, nullable=True)
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "description": self.description,
+            "count_ctas": self.count_ctas,
+            "count_bumps": self.count_bumps,
         }
